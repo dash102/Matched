faf582e9a1559a773e8f18c2dfddba1b78668a03
Stephanie Fu <stephaniefu@rocketmail.com>
2018-01-28 04:42:27 -0600

    Query weather, extract data

--- static/js/main.js
+++ static/js/main.js
@@ -1,84 +1,31 @@
+var HI = require('heat-index');
 
-function isAuthorized(){
-    return ((getPerpetrator() != '') &
-            (getName() != '') &
-            (getEmail() != '') &
-            (getSituation() != ''));
+const key = "f123ec11eb36daac9c3f5ef9a26c5ab6";
+function findWeather(zipcode) {
+    $.getJSON("http://api.openweathermap.org/data/2.5/forecast?zip=" + zipcode + "&APPID=" + key ,function(json){
+
+        var output = JSON.stringify(json, null, 2);
+        var jsonParsed = JSON.parse(output);
+
+        //console.log(typeof(jsonParsed.list[34].rain["3h"]));
+        console.log(jsonParsed);
+        for (var i = 0; i < 40; i++) {
+            var temperature = ((jsonParsed.list[i].main.temp) * (9.0/5.0)) - 459.67;
+            var humidity = jsonParsed.list[i].main.humidity;
+            var rain = typeof(jsonParsed.list[i].rain) === "undefined" || typeof(jsonParsed.list[i].rain["3h"]) === "undefined" ? 0 : parseFloat(jsonParsed.list[i].rain["3h"]);
+            var snow = typeof(jsonParsed.list[i].snow) === "undefined" || typeof(jsonParsed.list[i].snow["3h"]) === "undefined" ? 0 : parseFloat(jsonParsed.list[i].snow["3h"]);
+            var precipitation = rain + snow;
+
+            var heatIndex = HI.heatIndex({temperature: temperature, humidity: humidity, fahrenheit: true});
+            console.log(jsonParsed.list[i].dt_txt + ": ");
+            console.log("Heat index: " + heatIndex);
+            console.log("Precipitation: " + precipitation);
+            console.log();
+
+        }
+    });
 }
 
-function getPerpetrator(){
-    return $('#perpetrator-description').val();
-}
-
-function getEvidence(){
-    return $('#evidence-description').val();
-}
-
-function getSituation(){
-    return $('#situation-description').val();
-}
-
-function getName(){
-    return $('#name').val();
-}
-
-function getEmail(){
-    return $('#email').val();
-}
-
-function getNumber(){
-    return $('#number').val();
-}
-
-function getAdditional(){
-    return $('#additional').val();
-}
-
-permissions = {
-    'police': false,
-    'harvard': false,
-    'employers': false
-}
-
-function toggle(input){
-    console.log('toggle called')
-    permissions[input] = !permissions[input]
-}
-
-
-function formSubmit(){
-    console.log('about to ajax');
-    if (isAuthorized()){
-        var json = {
-            accuser: getName(),
-            email: getEmail(),
-            situation: getSituation(),
-            evidence: getEvidence(),
-            perpetrator: getPerpetrator(),
-            police: permissions['police'],
-            harvard: permissions['harvard'],
-            employers: permissions['employers']
-        };
-        $.ajax({
-            type: 'POST',
-            contentType: 'application/json; charset=utf-8',
-            data: JSON.stringify(json),
-            url: '/accuse/',
-            success: function(data){},
-            failure: function(result){
-            error();
-            }
-        });
-        // redirect to thank you
-        window.location.href = '/thanks';
-    }
-    else {
-        // TODO: Say with whom the conversation was.
-        $('.error-warn').html('All fields must be filled out.');
-    }
-}
 $(document).ready(function(){
-    document.getElementById("submit").onclick = function (){
-        formSubmit()
-    };
-})
+    findWeather("66503");
+});
